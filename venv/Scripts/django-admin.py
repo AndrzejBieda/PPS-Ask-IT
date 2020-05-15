0f9d78d6ff543549a560d5509e6e3d259769d66a
AndrzejBieda <andrzej.b116@gmail.com>
2020-05-15 16:37:48 +0200

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\andrz\Documents\Projekty\PycharmProjects\PPS_Ask_IT\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
