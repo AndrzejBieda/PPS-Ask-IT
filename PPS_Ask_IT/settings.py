84749854ff96880568e193f1755a46e1fd06d3a4
AndrzejBieda <andrzej.b116@gmail.com>
2020-05-15 21:29:44 +0200

    zmiana bazy na postgresql

--- PPS_Ask_IT/settings.py
+++ PPS_Ask_IT/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'PPS_Ask_IT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'askit'),
+        'USER': os.environ.get('DB_USER', 'askit'),
+        'PASSWORD': os.environ.get('DB_PASS', 'askit'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
