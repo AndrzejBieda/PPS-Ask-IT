65b73108a16a13976bdfdf64ad9420b5d4824de6
adrianalbrycht <trustek@o2.pl>
2020-06-02 18:22:39 +0200

    Pierwszy widok

--- Ask_IT/views.py
+++ Ask_IT/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# def homepage(response):
+#     return render(request=cos, template_name="base.html")
+
+def base(request):
+    return render(request, "Ask_IT/base.html")
