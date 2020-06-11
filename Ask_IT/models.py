4dfac9993e9dd34834ae93f7ffa16afe2bdf8768
adrianalbrycht <trustek@o2.pl>
2020-06-11 18:18:23 +0200

    new models

--- Ask_IT/models.py
+++ Ask_IT/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 # Create your models here.
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class UserAdditional(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    reputation = models.IntegerField(default=0)
 
     # def __str__(self):
     #     return self.avatar
@@ -17,5 +18,26 @@ class Category(models.Model):
     description = models.TextField(max_length=100)
     parent = models.ForeignKey("self", on_delete=models.CASCADE)
 
+
 # tabela wątek -> odnośnik do kategorii i osoby i inne pierdoły
 # tabela post (odpowiedź)
+
+# tabela question: tytuł, treść, id_autora, data, id_kategorii, id_najlepszej_odpowiedzi
+# tabela answer: treść, id_autora, data, id_pytania
+# dorobić do usera kolumnę reputacja
+
+
+class Question(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # best_answer = models.ForeignKey(Answer, on_delete=models.CASCADE())
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
